4036f0c4ab86d171c40bd64448a2e794b948a637
AhmedSeef <ahmed.seef.gooda@gmail.com>
2023-11-22 22:38:19 +0200

    edit sahred layer

--- LibrarySystem.Shared/DTOs/AuthorWithBooksDto.cs
+++ LibrarySystem.Shared/DTOs/AuthorWithBooksDto.cs
@@ -1,9 +1,4 @@
 ﻿using LibrarySystem.Shared.DTOs.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LibrarySystem.Shared.DTOs
 {
--- LibrarySystem.Shared/DTOs/BookDto.cs
+++ LibrarySystem.Shared/DTOs/BookDto.cs
@@ -3,7 +3,7 @@
 namespace LibrarySystem.Shared.DTOs
 {
     public class BookDto : BaseEntityDto
-    { 
+    {
         public int AuthorId { get; set; }
         public int PublisherId { get; set; }
     }
